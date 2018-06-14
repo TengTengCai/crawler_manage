5efab8134a98561f1cfc2599dab6f8e2f1d4bfef
Administrator <1021766585@qq.com>
2018-06-14 19:41:59 +0800

    model文件的编写

--- app/models.py
+++ app/models.py
@@ -1,7 +1,19 @@
 from app import db
+from datetime import datetime
 
 
-class User(db.Model):
-    u_id = db.Column(db.In)
-    username = db.Column(db.Integer, unique=True)
-    password = db.Column(db.String(225), nullable=False)
+class BaseModel(db.Model):
+    create_time = db.Column(db.DateTime, default=datetime.now)
+    update_time = db.Column(db.DateTime, onupdate=datetime.now)
+
+
+class User(BaseModel):
+    u_id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True)  # ID
+    username = db.Column(db.String(20), unique=True)  # 用户名
+    password = db.Column(db.String(225), nullable=False)  # 密码
+    nike_name = db.Column(db.String(20), unique=True)  # 昵称
+
+    __tablename__ = 'cm_users'
+
+    def __repr__(self):
+        return f'<User u_id:{self.u_id}, username:{self.username}>'
