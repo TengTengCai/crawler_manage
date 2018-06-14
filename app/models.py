04b487ee3bc4e2561233e54b2a2ed5594688dab0
Administrator <1021766585@qq.com>
2018-06-14 19:08:10 +0800

    model文件的编写

--- app/models.py
+++ app/models.py
@@ -2,5 +2,6 @@ from app import db
 
 
 class User(db.Model):
+    u_id = db.Column(db.In)
     username = db.Column(db.Integer, unique=True)
-
+    password = db.Column(db.String(225), nullable=False)
